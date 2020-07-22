1272a304f994b337ef73cac3e285cdba47daf0d9
Stephen <stephen.yin@outlook.com>
2020-07-22 23:34:08 +0800

    add http-server

--- src/http-server/index.js
+++ src/http-server/index.js
@@ -1,24 +1,52 @@
+import http from 'http'
+import url from 'url'
+
 export class App {
   constructor() {
     this.server = this.init()
     this.routeHandler = {
-      '/': xxx
+      '*': {
+        'GET': this.notFound
+      }
     }
   }
 
-  static createServer() {
+  static createApp() {
+    const cls = this
     const app = new cls()
-    return app.server
+    return app
   }
 
   init() {
     return http.createServer((req, res) => {
-      const pathname = url.parse(req)
-      this.routeHandler[pathname](req, res)
+      const pathname = url.parse(req.url).pathname
+      const method = req.method.toUpperCase()
+      if (typeof this.routeHandler?.[pathname]?.[method] === 'function') {
+        this.routeHandler[pathname][method](req, res)
+      } else {
+        this.routeHandler['*'].GET(req, res)
+      }
     })
   }
 
-  route(pathname, callback) {
-    this.routeHandler[pathname] = callback
+  route(pathname, method, callback) {
+    if (typeof this.routeHandler[pathname] === 'object') {
+      this.routeHandler[pathname][method.toUpperCase()] = callback
+    } else {
+      this.routeHandler[pathname] = {
+        [method.toUpperCase()]: callback,
+      }
+    }
+  }
+
+  listen(...args) {
+    this.server.listen(...args)
+  }
+
+  notFound(req, res) {
+    res.writeHead(404, { "Content-Type": "text/html" })
+    res.write("404 Not found")
+    res.end()
   }
 }
+
